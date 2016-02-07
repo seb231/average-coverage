f10070e183fa936815d64c16efb1a27269a0f206
seb231 <s.e.bailey@qmul.ac.uk>
2016-02-07 03:28:39 +0000

    WIP means across groups

--- src/average_coverage/core.clj
+++ src/average_coverage/core.clj
@@ -1,10 +1,21 @@
 (ns average-coverage.core
   (:require [clojure-csv.core :as csv]
             [clojure.string :as str]
-            [clojure.walk :as walk]))
+            [clojure.walk :as walk]
+            [schema.core :as s]
+            [schema.coerce :as coerce]))
 
 (set! *print-length* 10)
 (set! *warn-on-reflection* true)
+
+(def dataSchema
+  {:percentage_coverage Double
+   :length s/Num
+   s/Keyword s/Any})
+
+(defn coercion [schema data]
+  (let [coerce-data-fn (coerce/coercer schema coerce/string-coercion-matcher)]
+    (coerce-data-fn data)))
 
 (defn load-csv
   [filename]
@@ -16,7 +27,9 @@
         headers (->> (first parsed-csv)
                      (map str/lower-case)
                      (map #(str/replace % #"\s" ".")))]
-    (map #(walk/keywordize-keys (zipmap headers %1)) parsed-data)))
+    (->> (map #(walk/keywordize-keys (zipmap headers %1)) parsed-data)
+         (map #(coercion dataSchema %))
+         )))
 
 (defn write-csv
   [seq-of-maps filename-string]
@@ -25,4 +38,20 @@
         csv    (csv/write-csv data-to-csv)]
     (spit filename-string csv)))
 
+(defn group-by-gene [data] 
+  (group-by #(select-keys % [:gene]) data))
 
+(defn mean [xs]
+  (/ (reduce + xs)
+     (count xs)))
+
+(defn sum-over-grouped-map [keys grouped-data]
+  (map (fn [[key value]]
+         (merge key (mean (map #(keys %) value)))) grouped-data))
+
+(defn -main []
+  (->> (load-csv "data/pulldown.coverage.csv")
+       (group-by-gene)
+       (map #(map (fn [seb] (map (fn [here] (:percentage_coverage here)) seb)) %))
+       ;;(map #(mean (map (fn [[key value]] (:percentage_coverage value) %))))
+       ))
