164af9f399834606de0089cf68e160ea6fd87879
seb231 <s.e.bailey@qmul.ac.uk>
2016-02-16 15:40:06 +0000

    Output mean coverage

--- src/average_coverage/core.clj
+++ src/average_coverage/core.clj
@@ -3,9 +3,14 @@
             [clojure.string :as str]
             [clojure.walk :as walk]
             [schema.core :as s]
-            [schema.coerce :as coerce]))
+            [schema.coerce :as coerce]
+            [clojure.java.io :as io]))
 
-(set! *print-length* 10)
+
+(comment (use 'clojure.java.io)
+(with-open [wrtr (writer "test.txt")]
+  (.write wrtr (str (-main)))))
+
 (set! *warn-on-reflection* true)
 
 (def dataSchema
@@ -28,30 +33,21 @@
                      (map str/lower-case)
                      (map #(str/replace % #"\s" ".")))]
     (->> (map #(walk/keywordize-keys (zipmap headers %1)) parsed-data)
-         (map #(coercion dataSchema %))
-         )))
-
-(defn write-csv
-  [seq-of-maps filename-string]
-  (let [headers (map name (-> seq-of-maps (first) keys))
-        data-to-csv   (cons headers (map #(map str (vals %)) seq-of-maps))
-        csv    (csv/write-csv data-to-csv)]
-    (spit filename-string csv)))
+         (map #(coercion dataSchema %)))))
 
 (defn group-by-gene [data] 
   (group-by #(select-keys % [:gene]) data))
 
-(defn mean [xs]
-  (/ (reduce + xs)
-     (count xs)))
-
-(defn sum-over-grouped-map [keys grouped-data]
-  (map (fn [[key value]]
-         (merge key (mean (map #(keys %) value)))) grouped-data))
-
 (defn -main []
-  (->> (load-csv "data/pulldown.coverage.csv")
-       (group-by-gene)
-       (map #(map (fn [seb] (map (fn [here] (:percentage_coverage here)) seb)) %))
-       ;;(map #(mean (map (fn [[key value]] (:percentage_coverage value) %))))
-       ))
+  (let [grouped-data (->> (load-csv "data/pulldown.coverage.csv")
+              (map #(select-keys % [:gene :percentage_coverage]))
+              (group-by-gene))
+        genes (->> (map first grouped-data)
+                   (map :gene))
+        lengths (->> grouped-data
+                     (map #(second %)) 
+                     (map #(vec (map :percentage_coverage %))))
+        mean-coverage (map #(double (/ (reduce + %)
+                                      (count %))) lengths)]
+    (zipmap genes mean-coverage)))
+
